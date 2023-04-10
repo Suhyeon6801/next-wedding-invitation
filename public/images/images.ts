b55df1243bd827d94ae2ec0f0de36376dd5a67e3
ssuhyeon-kim <ssuhyeon.kim@navercorp.com>
2023-04-10 22:52:34 +0900

    chore: add sample images

---
+++ public/images/images.ts
@@ -0,0 +1,8 @@
+import CAT1 from '../images/cat1.jpeg';
+import CAT2 from '../images/cat2.jpeg';
+import CAT3 from '../images/cat3.jpeg';
+import CAT4 from '../images/cat4.jpeg';
+import CAT5 from '../images/cat5.jpeg';
+import CAT6 from '../images/cat6.jpeg';
+
+export const CATS_IMAGES = [CAT1, CAT2, CAT3, CAT4, CAT5, CAT6];
