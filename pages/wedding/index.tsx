2f783a55a11a3b437ea9039d67f7c0d0747cfceb
seunghyun3309 <seunghyun3309@gmail.com>
2023-04-23 14:48:36 +0900

    Update index.tsx
    main frame add

--- pages/wedding/index.tsx
+++ pages/wedding/index.tsx
@@ -5,18 +5,36 @@ import { Photo } from '@/components/Photo';
 import { Account } from '@/components/Account';
 import { MapInfo } from '@/components/MapInfo';
 import { GuestBook } from '@/components/GuestBook';
+import styled from '@emotion/styled';
 
 const IndexPage: NextPage = () => {
   return (
     <>
+    <MainFrame>
       <Intro />
       <Photo />
       <MapInfo />
       <Account />
       <GuestBook />
       <Footer />
+    </MainFrame>
     </>
   );
 }
+
+const MainFrame = styled.div`
+  margin-top: 50px;
+  margin-bottom: 50px;
+  padding-left: 20px;
+  padding-right: 20px;
+  text-align: center;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 0;
+  width: 800px;
+  height: 1423px;
+  style="position:absolute; top:0px; left: 0px color: #f7f7f7;
+`;
 
 export default IndexPage;
