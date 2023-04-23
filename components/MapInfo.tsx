930b98c2ab9510e0aaf8bea074b76865887d7ef4
ssuhyeon-kim <ssuhyeon.kim@navercorp.com>
2023-04-23 14:43:41 +0900

    feat: open map

--- components/MapInfo.tsx
+++ components/MapInfo.tsx
@@ -1,7 +1,14 @@
+import goToKakaoMap from '@/utils/goToKakaoMap';
+import styled from '@emotion/styled';
 import Script from 'next/script';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=61020e30c2d3c34412987eaf7487a51d&autoload=false`;
+
+const Location = {
+  lat: 33.5563,
+  lng: 126.79581,
+}
 
 export const MapInfo = () => {
   return (
@@ -9,13 +16,23 @@ export const MapInfo = () => {
       <h1>오시는 길</h1>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
       <Map
-        center={{ lat: 33.5563, lng: 126.79581 }}
+        center={{ lat: Location.lat, lng: Location.lng }}
         style={{ width: "100%", height: "360px" }}
+        level={3}
       >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000000" }}>Hello World!</div>
-      </MapMarker>
+      <MapMarker 
+        position={{ lat: Location.lat, lng: Location.lng }}
+        onClick={() => goToKakaoMap(Location.lat, Location.lng)}
+      />
       </Map>
+      <GoToMap onClick={() => goToKakaoMap(Location.lat, Location.lng)}>지도 바로가기</GoToMap>
     </>
   );
 }
+
+const GoToMap = styled.div`
+  margin: 10px;
+  padding: 20px;
+  color: #000;
+  background: #00ff00;
+`;
