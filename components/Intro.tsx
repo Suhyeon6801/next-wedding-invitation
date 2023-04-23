a5da2e3897d4ed4ca394a315ee64eb2a0eb7e3cb
ssuhyeon-kim <ssuhyeon.kim@navercorp.com>
2023-04-23 21:18:02 +0900

    fix: draft5

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,14 +3,32 @@ import Image from 'next/image'
 import styled from '@emotion/styled';
 import CAT7 from '../public/images/cat7.jpeg';
 import FLOWER from '../public/images/flower.png'
-import { Autour_One } from '@next/font/google';
+import MAIN from '../public/images/main_sample.jpeg';
 
 export const Intro = () => {
     return (
       <>
-        <Header>SeungHyun & Suhyeon Wedding Card</Header>
-        <TitleSection />
-        <MainPhoto />
+        {/* <Header>SeungHyun & Suhyeon Wedding Card</Header> */}       
+        <ImageWithText>
+          <div className="photo">
+            <Image 
+              src={MAIN} 
+              layout="responsive" 
+              alt="이미지" 
+              style={{
+                display:'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                objectFit: 'cover'
+            }} />
+          </div>
+           <div className="text-container">
+            <span>박혜진, 박동현</span>
+            <span style={{fontSize: '0.5em', marginTop: '10px'}}>우리의 결혼식에 초대합니다</span>
+          </div>
+        </ImageWithText>
+        {/* <TitleSection />
+        <MainPhoto /> */}
         <TimeSpaceSection />
         <GreetingText />
         <FromInfo />
@@ -20,7 +38,7 @@ export const Intro = () => {
 
 const TitleSection = () => {
   return (
-    <Title>이승현 & 김수현</Title>
+    <Title>박혜진 & 박동현</Title>
   )
 }
 
@@ -83,6 +101,36 @@ const FromInfo = () => {
   )
 }
 
+
+const ImageWithText = styled.div`
+  position: relative;
+  display: inline-block;
+
+  .photo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text-container {
+    position: absolute;
+    margin-top: 20px;
+    top: 10px;
+    right: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    font-size: 1em;
+    font-weight: bold;
+    color:#ffffff;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  }
+
+  .text-container span {
+    margin-bottom: 5px;
+  }
+`;
+
 const Header = styled.header`
   padding: 20px;
   font-size: 1.2em;
@@ -99,7 +147,6 @@ const PhotoContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  object-fit:cover;
 `
 
 const TimeSpace = styled.h3`
