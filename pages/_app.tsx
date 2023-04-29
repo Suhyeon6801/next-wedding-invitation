1be4e284ba8b3e8be226c97c5425481b6ee28828
ssuhyeon-kim <ssuhyeon.kim@navercorp.com>
2023-04-29 19:23:23 +0900

    feat: add twitter open graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,17 +14,24 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
      <Head>
-          <meta name="description" content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀" />
-          <meta
-            property="og:description"
-            content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀"
-          />
-          <meta property="og:image" content="./meta.jpeg" />
-          <meta property="og:title" content="박동현, 박혜진 결혼합니다" />
-          <meta
-            property="og:url"
-            content="https://next-wedding-invitation.vercel.app/"
-          />
+        <meta name="description" content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀" />
+        <meta
+          property="og:description"
+          content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀"
+        />
+        <meta property="og:image" content="./meta.jpeg" />
+        <meta property="og:title" content="박동현, 박혜진 결혼합니다" />
+        <meta
+          property="og:url"
+          content="https://next-wedding-invitation.vercel.app/"
+        />
+        {/* twitter open graph */}
+        <meta name="twitter:title" content="박동현, 박혜진 결혼합니다" />
+        <meta name="twitter:image" content="./meta.jpeg" />
+        <meta
+          name="twitter:url"
+          content="https://next-wedding-invitation.vercel.app/"
+        />
       </Head>
       <Global styles={GlobalFontStyle} />
       <Component {...pageProps} />
