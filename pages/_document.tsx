f1baca2b77ad1df68a66aa859d37fbf05a605211
ssuhyeon-kim <ssuhyeon.kim@navercorp.com>
2023-04-30 02:13:36 +0900

    fix: favicon 변경, open graph document로 이동  953f67

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,27 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀" />
+        <meta
+          property="og:description"
+          content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀"
+        />
+        <meta property="og:image" content="./meta.png" />
+        <meta property="og:title" content="박동현, 박혜진 결혼합니다" />
+        <meta
+          property="og:url"
+          content="https://next-wedding-invitation.vercel.app/"
+        />
+        {/* twitter open graph */}
+        <meta name="twitter:title" content="박동현, 박혜진 결혼합니다" />
+        <meta name="twitter:image" content="./meta.png" />
+        <meta
+          name="twitter:url"
+          content="https://next-wedding-invitation.vercel.app/"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body style={{margin: '0px', padding: '0px'}}>
         <Main />
         <NextScript />
