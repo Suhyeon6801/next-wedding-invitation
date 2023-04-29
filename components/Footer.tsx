a06ea36c7d259c4d5482391c9b86240363244f7e
ssuhyeon-kim <ssuhyeon.kim@navercorp.com>
2023-04-29 18:07:36 +0900

    fix: footer margin 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,11 @@
 import styled from '@emotion/styled';
 
 export const Footer = () => {
-  return (
-    <FooterCover>소중한 분을 초대합니다</FooterCover>
-  )
+  return  <FooterCover>소중한 분을 초대합니다</FooterCover>;
 }
 
 const FooterCover = styled.div`
   margin: 20px 0px 0px 0px;
-  padding: 30px;
+  padding: 25px;
   background-color: #f5f5f5;
 `
