f91356c41a811d9e96eb299cbfda69d285aeede9
ssuhyeon-kim <ssuhyeon.kim@navercorp.com>
2023-04-29 19:10:36 +0900

    fix: og meta move to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { Global, css } from '@emotion/react';
+import Head from 'next/head';
 
 const GlobalFontStyle =  css`
   @import url('https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&display=swap');
@@ -12,6 +13,19 @@ const GlobalFontStyle =  css`
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+     <Head>
+          <meta name="description" content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀" />
+          <meta
+            property="og:description"
+            content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀"
+          />
+          <meta property="og:image" content={"https://next-wedding-invitation.vercel.app//meta.jpeg"} />
+          <meta property="og:title" content="박동현, 박혜진 결혼합니다" />
+          <meta
+            property="og:url"
+            content="https://next-wedding-invitation.vercel.app/"
+          />
+      </Head>
       <Global styles={GlobalFontStyle} />
       <Component {...pageProps} />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,11 @@ import { Photo } from '@/components/Photo';
 import { Account } from '@/components/Account';
 import { MapInfo } from '@/components/MapInfo';
 import styled from '@emotion/styled';
-import Head from 'next/head';
-import META_IMAGE from '@/public/images/wedding_hor1.jpeg';
 
 const IndexPage: NextPage = () => {
   return (
     <>
       <Background>
-        <Head>
-          <meta name="description" content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀" />
-          <meta
-            property="og:description"
-            content="6월 10일 토요일 오후 1시 10분, 더 베네치아 3층 베네치아홀"
-          />
-          <meta property="og:image" content="./meta.jpeg" />
-          <meta property="og:title" content="박동현, 박혜진 결혼합니다" />
-          <meta
-            property="og:url"
-            content="https://next-wedding-invitation.vercel.app/"
-          />
-        </Head>
         <Intro />
         <Photo />
         <MapInfo />
